c5e8da47dd63e56c49b30a8aeffd924f2d7fe0d5
Lais Frigerio <laisfrigerio@hotmail.com>
2022-01-26 06:27:05 -0300

    Adding default image when user from github doesn't exists

--- pages/index.js
+++ pages/index.js
@@ -70,10 +70,15 @@ function Background ({ children }) {
 }
 
 function Image ({ src, ...props}) {
+    const addDefaultSrc = (event) => {
+        event.target.src = 'https://via.placeholder.com/150';
+    }
+
     return (
         <img
             {...props}
             src={src}
+            onError={addDefaultSrc}
         />
     );
 }
